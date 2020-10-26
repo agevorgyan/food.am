5681be6c5bfcf475b749d3082269164fbc494f12
AVET <avet.gevorgyan@gmail.com>
2020-10-26 16:06:23 +0400

    123w

--- js/script.js
+++ js/script.js
@@ -257,6 +257,10 @@ window.addEventListener('DOMContentLoaded', () => {
                 if (request.status === 200) {
                     console.log(request.response);
                     statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(()=>{
+                        statusMessage.remove();
+                    });
 
                 } else {
                     statusMessage.textContent = message.fail;
